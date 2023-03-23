6522c675ae7d848f8ddb2a4fa4d2939b22a9d25c
Vladislav <scroynel@gmail.com>
2023-03-23 19:49:09 +0100

    drf-yasg documentation

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import GamesList, GamesDetail, ReviewCreateView, AddStarRatingView
+from .yasg import urlpatterns as dir_urls
 
 urlpatterns = [
     path('games', GamesList.as_view(), name='games'),
@@ -8,3 +9,5 @@ urlpatterns = [
     path('review/', ReviewCreateView.as_view(), name= 'create-review'),
     path('rating/', AddStarRatingView.as_view(), name= 'create-rating')
 ]
+
+urlpatterns += dir_urls
